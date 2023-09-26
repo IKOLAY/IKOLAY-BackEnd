125d2e7bacaaaba5f4ab94598ff88d8b9f2de6a5
Adanedhel <selimfurkangulnihal@gmail.com>
2023-09-26 15:16:59 +0300

    26 / 09 Şirket getirme ile ilgili backend kodları yazıldı düzenleme getirildi

--- company-service/src/main/java/com/ikolay/mapper/ICompanyMapper.java
+++ company-service/src/main/java/com/ikolay/mapper/ICompanyMapper.java
@@ -2,10 +2,12 @@ package com.ikolay.mapper;
 
 import com.ikolay.dto.requests.RegisterRequestDto;
 import com.ikolay.dto.response.ConfirmationInfoResponseDto;
+import com.ikolay.dto.response.GetTop5ForCompanyResponseDto;
 import com.ikolay.repository.entity.Company;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -15,4 +17,6 @@ public interface ICompanyMapper {
     Company toCompany(final RegisterRequestDto dto);
 
     List<ConfirmationInfoResponseDto> toConfirmationResponseDtos(final List<Company> company);
+
+    List<GetTop5ForCompanyResponseDto> toGetTop5ForCompanyResponseDto(final List<Company> company);
 }
--- company-service/src/main/java/com/ikolay/service/CompanyService.java
+++ company-service/src/main/java/com/ikolay/service/CompanyService.java
@@ -2,12 +2,17 @@ package com.ikolay.service;
 
 import com.ikolay.dto.requests.RegisterRequestDto;
 import com.ikolay.dto.response.ConfirmationInfoResponseDto;
+import com.ikolay.dto.response.GetTop5ForCompanyResponseDto;
 import com.ikolay.exception.CompanyManagerException;
 import com.ikolay.exception.ErrorType;
 import com.ikolay.mapper.ICompanyMapper;
 import com.ikolay.repository.ICompanyRepository;
 import com.ikolay.repository.entity.Company;
 import com.ikolay.utility.ServiceManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -16,7 +21,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class CompanyService extends ServiceManager<Company,Long> {
+public class CompanyService extends ServiceManager<Company, Long> {
     private final ICompanyRepository companyRepository;
 
     public CompanyService(ICompanyRepository companyRepository) {
@@ -38,8 +43,13 @@ public class CompanyService extends ServiceManager<Company,Long> {
     }
 
     @PostConstruct
-    private void addDefaultCompany(){
+    private void addDefaultCompany() {
         save(Company.builder().companyName("dummyCorp").logo("3.jpg").address("somewhereonearth").about("weworkin").taxNo("123123").build());
+        save(Company.builder().companyName("ikolay").logo("2.jpg").address("somewhereonearth").about("kalpsizler").taxNo("3333").build());
+        save(Company.builder().companyName("tokinan a.ş").logo("4.jpg").address("letsgo").about("basliyoruuz").taxNo("44444").build());
+        save(Company.builder().companyName("gülnihal a.ş").logo("5.jpg").address("hi").about("seskes").taxNo("55555").build());
+        save(Company.builder().companyName("martlı a.ş").logo("6.jpg").address("guerillamountain").about("armed").taxNo("66666").build());
+        save(Company.builder().companyName("adanedhel a.ş").logo("7.jpg").address("middle-earth").about("rivendale").taxNo("88888").build());
     }
 
     public Company getCompanyInformation(Long id) {
@@ -59,5 +69,14 @@ public class CompanyService extends ServiceManager<Company,Long> {
     public List<ConfirmationInfoResponseDto> companyInfoForConfirmation(List<Long> companyIds) {
         List<Company> companies = companyRepository.findByIdIn(companyIds);
         return ICompanyMapper.INSTANCE.toConfirmationResponseDtos(companies);
+    }
+
+    public List<GetTop5ForCompanyResponseDto> findByCompanyNameTop5() {
+        Sort sort= Sort.by(Sort.Direction.fromString("ASC"), "companyName");
+        Pageable pageable = PageRequest.of(0,5,sort);
+        Page<Company> companies = companyRepository.findAll(pageable);
+        List<Company> companyList = companies.get().toList();
+
+        return ICompanyMapper.INSTANCE.toGetTop5ForCompanyResponseDto(companyList);
     }
 }
