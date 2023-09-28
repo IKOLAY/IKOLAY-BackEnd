261f439e880889128173ef8ae1598a19cbd3e619
Adanedhel <selimfurkangulnihal@gmail.com>
2023-09-28 13:41:29 +0300

    Güncellenmiş ENDPOINTLERI BİR DAHA GÜNCELLEME İŞLEMİ

--- auth-service/src/main/java/com/ikolay/constant/EndPoints.java
+++ auth-service/src/main/java/com/ikolay/constant/EndPoints.java
@@ -5,15 +5,11 @@ public class EndPoints {
         public static final String AUTH = VERSION + "/auth";
         public static final String REGISTER = "/register";
         public static final String LOGIN = "/login";
-
         public static final String UPDATE = "/update";
         public static final String DELETE = "/delete";
         public static final String FINDCOMPANY = "/findcompany";
-        public static final String DELETEBYID = "/delete/{id}";
-        public static final String FINDALL = "/findall";
         public static final String ACTIVATION = "/activation";
         public static final String CONFIRMATION = "/confirmation";
-
         public static final String APPROVE = "/approve";
         public static final String DELETEFROMCONFIRMATION = "/deletewithauthid";
 
--- company-service/src/main/java/com/ikolay/constant/EndPoints.java
+++ company-service/src/main/java/com/ikolay/constant/EndPoints.java
@@ -7,20 +7,16 @@ public class EndPoints {
     public static final String TRANSACTION = VERSION + "/transaction";
     public static final String LEAVE = VERSION + "/leave";
     public static final String COMMENT = VERSION + "/comment";
-    public static final String ADDCOMMENT = VERSION + "/addcomment";
+    public static final String ADDCOMMENT = "/addcomment";
     public static final String REGISTER = "/register";
-    public static final String LOGIN = "/login";
     public static final String UPDATE = "/update";
     public static final String FINDCOMPANY = "/findcompany";
-
     public static final String ADD = "/add";
     public static final String CREATE = "/create";
-    public static final String DELETEBYID = "/delete/{id}";
     public static final String DELETE = "/delete";
     public static final String GETCOMPANYINFORMATION = "/companyinformation";
     public static final String COMPANYINFOFORCONFIRMATION = "/getcompanynameandtaxno";
     public static final String FINDBYCOMPANYNAMETOP5 = "/findbycompanynametopfive";
-
     public static final String FINDALL = "/findall";
     public static final String INCOMINGPAYMENTS = "/payments";
     public static final String PROFITLOSS = "/annualprofitloss";
